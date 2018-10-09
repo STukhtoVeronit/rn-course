7307696ff98ba4e8d06b083e4f1229e789bc5a78
STukhtoVeronit <s.tukhto.vironit@vironit.ru>
2018-10-09 18:18:21 +0300

    Created react-native project

---
+++ AwesomeProject/src/Nav/nav.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import {Platform, StyleSheet, Text, View} from 'react-native';
+
+class Nav extends React.Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {};
+
+	}
+
+	render() {
+		return (
+				<div>
+
+				</div>
+		);
+	}
+}
+
+Nav.propTypes = {};
+
+export default Nav;
