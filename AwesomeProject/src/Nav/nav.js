cf64e81094640529903dd27d7dfddcaea24d4962
STukhtoVeronit <s.tukhto.vironit@vironit.ru>
2018-10-10 16:19:19 +0300

    Created default react-native app

--- AwesomeProject/src/Nav/nav.js
+++ AwesomeProject/src/Nav/nav.js
@@ -1,7 +1,11 @@
+/**
+ * @format
+ * @flow
+ */
 import React from 'react';
 import {Platform, StyleSheet, Text, View} from 'react-native';
 
-class Nav extends React.Component {
+class Nav extends React.Component<props> {
 	constructor(props) {
 		super(props);
 
@@ -11,13 +15,24 @@ class Nav extends React.Component {
 
 	render() {
 		return (
-				<div>
-
-				</div>
+				<View style={styles.nav}>
+					<Text>{this.props.nameOfApp}</Text>
+				</View>
 		);
 	}
 }
 
 Nav.propTypes = {};
+
+const styles = StyleSheet.create({
+	nav: {
+		alignItems: 'center',
+		width: '100%',
+		padding: 10,
+		borderWidth: 2,
+		borderColor: '#eaeaea',
+		backgroundColor: '#dddddd',
+	}
+});
 
 export default Nav;
